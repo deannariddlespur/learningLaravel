2a954755749e8f651f2aa5bb92eb5a695b5e0adb
webdevdea <webdevdea@gmail.com>
2014-09-10 19:06:13 -0400

    style.css creation

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,13 +1,28 @@
+<!--default layout-->
 <!doctype html>
 <html lang="en">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>learningLaravel</title>
 <head>
-	<meta charset=UTF-8>
-	<title> Learning Laravel Website </title>
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootsrap.min.css">
-	<script src="https://code.jquery.com/jquery.js"></script>
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+ 
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+ 
+<!-- Optional theme -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+ 
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+
+
 </head>
 <body>
+		<div class="container col-md-8 col-md-offset-2">
+		<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+
 	<nav class="navbar navbar-default" role="navigation">
 	   <div class="navbar-header">
 	   	<ul class="nav navbar-nav">
@@ -19,6 +34,8 @@
   </nav>
 	   	
 @yield('content')
+</div>
+
 </body>
 </html>
 		
