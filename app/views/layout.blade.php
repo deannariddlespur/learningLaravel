38c8ab671a4d0c19da0ebb39589e85774fbbf7c0
webdevdea <webdevdea@gmail.com>
2014-09-10 18:41:25 -0400

    Trying to bootstrap

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -3,13 +3,21 @@
 <head>
 	<meta charset=UTF-8>
 	<title> Learning Laravel Website </title>
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootsrap.min.css">
+	<script src="https://code.jquery.com/jquery.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<ul>
-		<li><a href="./">Home</a></li>
-		<li><a href="./about">About</a></li>
-		<li><a href="./contact">Contact</a></li>
-	</ul>
+	<nav class="navbar navbar-default" role="navigation">
+	   <div class="navbar-header">
+	   	<ul class="nav navbar-nav">
+	   		<li class="active"> <a href="./">Home</a></li>
+	   		<li><a href="./about">About</a></li>
+	   		<li><a href="./contact">Contact</a></li>
+	   	 </ul>
+	 </div>
+  </nav>
+	   	
 @yield('content')
 </body>
 </html>
